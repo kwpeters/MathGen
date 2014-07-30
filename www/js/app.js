f7ddb06eff8bf7dfe4fa134465276df732543c51
Kevin Peters <kwpeters@ra.rockwell.com>
2014-07-29 21:49:05 -0400

    Added node server.

--- www/js/app.js
+++ www/js/app.js
@@ -23,7 +23,7 @@ angular.module(
 
             $stateProvider.state('home', {
                 url: '/home',
-                templateUrl: 'views/home.html'//,
+                templateUrl: 'js/views/home.html'//,
                 //controller: 'HomeCtrl'
             });
 
