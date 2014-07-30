1940e0cef2676687d49c5171ca228ba1cdd8dd66
Kevin Peters <kwpeters@ra.rockwell.com>
2014-07-29 22:06:52 -0400

    Removed server.js that was previously moved.

--- server.js
+++
@@ -1,8 +0,0 @@
-var connect = require('connect');
-
-var portNum = 3000;
-console.log("Server started on port %d.", portNum);
-
-var app = connect()
-          .use(connect.static(__dirname + '/www'))
-          .listen(portNum);
